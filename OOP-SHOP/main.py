7f1a2af29003795dc3934c271b9e84e9acdd8ddc
myrzaiym00 <myrzaiym.00.01@gmail.com>
2022-07-11 12:56:21 +0600

    urls

--- OOP-SHOP/main.py
+++ OOP-SHOP/main.py
@@ -1,17 +1,25 @@
-from shop.models import Product, Category
-from shop.views import product_list, product_create, product_detail,product_delete, product_update
-
-cat = Category("phones")
-Category("dyson")
-Category("food")
-
-Product("iphone", 234, "...", 3, cat)
-Product("lenovo", 34, "...", 5, cat)
-Product("samsung", 87, "...", 10, cat)
-
+from urls import url_patterns
 from pprint import pprint
-#pprint(product_create())
-pprint(product_list())
-id_ = input("Введите продукт для обновления: ")
-pprint(product_update(id_))
-pprint(product_list())
+
+while True:
+    try:
+        url, arg = input("Введите адрес: ").split("/")
+    except ValueError:
+        print("Enter a valid url")
+        continue
+
+    found = False
+    for uri, view in url_patterns:
+        if uri.split("/")[0] == url:
+            found = True
+
+            try:
+                if arg:
+                    pprint(view(arg))
+                else:
+                    pprint(view())
+            except Exception as e:
+                print(e)
+
+    if not found:
+        print("404 Url Not Found")
